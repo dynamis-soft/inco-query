d48938a09caeda5d29f3fd3e57b8c72a0fd53c1d
Gabriel Villalobos <info@dynamis-soft.com>
2016-10-25 22:39:19 -0600

    Adding sass and module

--- iq/app/app.module.js
+++ iq/app/app.module.js
@@ -1,3 +1,9 @@
-/**
- * Created by xnehi on 10/25/2016.
- */
+window.$ = require('jquery');
+window.jQuery = $;
+
+var angular = require('angular');
+var ng_bootstrap = require('angular-ui-bootstrap');
+var ng_router = require('angular-ui-router');
+
+angular.module('iq-web-app', [ng_bootstrap, ng_router]);
+
